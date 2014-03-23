15c53c0c717172851686a9c16f12b98d98891477
owenx <owenpragel@gmail.com>
2014-03-23 05:39:53 -0700

    Initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orails::Application.config.secret_key_base = '9fe4b269c1f5f03b72fcd5da89fae8daa48a9a9a8ed523cdc69357cc0cbd2936ae4b4ac189bfd90d75caf23aefcb502441f4eadb271029c2150e4709e000774b'
